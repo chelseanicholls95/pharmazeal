53e33eeaaf6cc526842fa1bbdb19a0838167188c
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2024-02-27 11:09:29 +0000

    set up customer routes

---
+++ src/app/customer/[customerId]/order/page.js
@@ -0,0 +1,5 @@
+const CustomerOrder = () => {
+  return <h1>Customer Order</h1>;
+};
+
+export default CustomerOrder;
---
+++ src/app/customer/login/page.js
@@ -0,0 +1,5 @@
+const CustomerLogin = () => {
+  return <h1>Customer Login</h1>;
+};
+
+export default CustomerLogin;
