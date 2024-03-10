7428a02110127c994f1bfd62b30a2a80f3409bac
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2024-03-10 14:46:09 +0000

    added react hook form to login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,10 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
 import "./loginform.css";
 
 const LoginForm = () => {
+  const form = useForm();
+  const { register, control, handleSubmit } = form;
+
   const router = useRouter();
   const [error, setError] = useState("");
   const session = useSession();
@@ -15,21 +21,16 @@ const LoginForm = () => {
     }
   }, [session, router]);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(event.target);
-    const username = event.target[0].value;
-    const password = event.target[1].value;
-
-    const res = await signIn("credentials", {
+  const onSubmit = async ({ username, password }) => {
+    const response = await signIn("credentials", {
       redirect: false,
       username,
       password,
     });
 
-    if (res?.error) {
+    if (response?.error) {
       setError("Invalid email or password");
-      if (res?.url) router.replace("/");
+      if (response?.url) router.replace("/");
     } else {
       setError("");
     }
@@ -37,13 +38,16 @@ const LoginForm = () => {
 
   return (
     <div className="m5">
-      <form className="form bg-dark text-white mt-5">
+      <form
+        className="form bg-dark text-white mt-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h1 className="text-center pt-3">Login Form</h1>
         <div className="pt-5 p-3">
           <input
             type="text"
             id="username"
-            name="username"
+            {...register("username")}
             placeholder="Username"
             className="form-control"
             required
@@ -53,7 +57,7 @@ const LoginForm = () => {
           <input
             type="password"
             id="password"
-            name="password"
+            {...register("password")}
             placeholder="Password"
             className="form-control"
             required
@@ -62,7 +66,13 @@ const LoginForm = () => {
         <div className="p-3 d-grid gap-2">
           <button className="btn btn-light btn-lg">Submit</button>
         </div>
+        {error ? (
+          <div className="text-center">
+            <p>{error}</p>
+          </div>
+        ) : null}
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
