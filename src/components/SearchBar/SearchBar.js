44df25ad528a3e029b66176828fae3b2ebd4a362
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2024-03-13 15:48:08 +0000

    added a button to reset customers

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 
 const SearchBar = ({ customers }) => {
   const form = useForm();
-  const { register, control, handleSubmit } = form;
+  const { register, handleSubmit } = form;
   const router = useRouter();
 
   const onSubmit = ({ searchInput }) => {
@@ -22,19 +22,34 @@ const SearchBar = ({ customers }) => {
     router.push(url);
   };
 
+  const onClick = () => {
+    router.push("/customers");
+  };
+
   return (
-    <div className="input-group mb-3 w-25 ms-5">
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search by Surname"
-          id="searchInput"
-          {...register("searchInput")}
-        />
-        <button className="btn btn-outline-secondary" type="submit">
-          Button
-        </button>
+    <div className="input-group mb-3 w-100 ms-5">
+      <form
+        className="form w-100 d-flex justify-content-around"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="d-flex w-25">
+          <input
+            type="text"
+            className="form-control "
+            placeholder="Search by Surname"
+            id="searchInput"
+            {...register("searchInput")}
+            required
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </div>
+        <div>
+          <button className="btn btn-danger " onClick={onClick}>
+            Reset Customers
+          </button>
+        </div>
       </form>
     </div>
   );
