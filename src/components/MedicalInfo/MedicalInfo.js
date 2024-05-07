4932f5d7af94fc5a0a84e1d29ae76996e94d1325
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2024-05-07 20:28:16 +0100

    displayed medical conditions and allergies in a table

--- src/components/MedicalInfo/MedicalInfo.js
+++ src/components/MedicalInfo/MedicalInfo.js
@@ -1,6 +1,16 @@
 import AddButtons from "@/components/AddButtons/AddButtons";
 
 const MedicalInfo = ({ id, allergies, medicalConditions }) => {
+  let length = 0;
+
+  if (allergies.length > 0 || medicalConditions > 0) {
+    if (allergies.length > medicalConditions.length) {
+      length = allergies.length;
+    } else {
+      length = medicalConditions.length;
+    }
+  }
+
   return (
     <div className="m-4 w-100 bg-dark text-light border border-dark rounded">
       <h3 className="m-3 text-center">Medical Info</h3>
@@ -13,7 +23,38 @@ const MedicalInfo = ({ id, allergies, medicalConditions }) => {
             </tr>
           </thead>
           <tbody>
-            <AddButtons id={id} />
+            <tr>
+              {medicalConditions.length === 0 ? (
+                <td>No medical conditions</td>
+              ) : (
+                <td>{medicalConditions[0]}</td>
+              )}
+              {allergies.length === 0 ? (
+                <td>No allergies</td>
+              ) : (
+                <td>{allergies[0]}</td>
+              )}
+            </tr>
+            {length > 1 ? (
+              <tr>
+                {medicalConditions.length > 0 ? (
+                  <td>{medicalConditions[1]}</td>
+                ) : (
+                  <td></td>
+                )}
+                {allergies.length > 0 ? <td>{allergies[1]}</td> : <td></td>}
+              </tr>
+            ) : null}
+            {length > 2 ? (
+              <tr>
+                {medicalConditions.length > 1 ? (
+                  <td>{medicalConditions[2]}</td>
+                ) : (
+                  <td></td>
+                )}
+                {allergies.length > 1 ? <td>{allergies[2]}</td> : <td></td>}
+              </tr>
+            ) : null}
           </tbody>
         </table>
       </div>
